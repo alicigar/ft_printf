e5d4a1bee10c497a8eae657515971d39f1a0f54c
Alicia Momoko Garcia Gokan <alicigar@c1r10s1.42malaga.com>
2025-01-13 20:16:09 +0100

    incompleted

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME = libftprint.a
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+SRCS = ft_printf.c 
+OBJS = $(SRCS:.c=.o)
+RM = rm -f
+LIB = ar rcs
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(LIB) $(NAME) $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alicigar < alicigar@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 17:26:17 by alicigar          #+#    #+#             */
+/*   Updated: 2025/01/13 20:05:45 by alicigar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+/*funciones
+para: %c, %s, %p, %d, %i, %u, %x, %X, %% */
+
+
+#endif
